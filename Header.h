b8502ca25a10185234bb46d9962f2b3818b99996
KatValentina <valentinakataeva90@gmail.com>
2025-01-04 00:05:49 +0500

    Задача №10
    Прикрепляю решение задачи под номером 10

---
+++ Header.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+using namespace std;
+
+//проверка на корректный ввод
+bool korrec(const string& a);
+
+//функция деления числа на 16
+void delenie(string& a, int& ost);
+
+//функция перевода числа в 16 с.с
+string perevod(string a);
